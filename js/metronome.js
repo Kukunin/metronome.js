b53f993258874db5f157e52bade4ee07262dcee1
Sergey Kukunin <sergey.kukunin@gmail.com>
2014-03-23 22:23:45 +0200

    Minimal working version, proof of concept

---
+++ js/metronome.js
@@ -0,0 +1,34 @@
+var Metronome;
+(function() {
+    "use strict";
+
+    Metronome = function(callback, bpm) {
+        this.callback = callback ? callback : function() {};
+
+        this.bpm = bpm ? bpm : 60;
+        this.frame = 1000 * 60 / this.bpm;
+    };
+
+    Metronome.prototype.start = function() {
+        this.runAt = new Date().getTime();
+        this.tick();
+    };
+
+    Metronome.prototype.tick = function() {
+        //Run original callback
+        this.callback();
+
+        //Calculate delay for setTimeout
+        var nextRunAt = this.runAt + this.frame,
+            delay = nextRunAt - (new Date().getTime()),
+            that = this;
+
+        setTimeout(function() {
+            that.tick.call(that);
+        }, delay);
+
+        this.runAt = nextRunAt;
+    };
+
+
+})();
